1210f65493471f78c861488a920244ee6946357a
glabmoris <guillaume.morissette@cidco.ca>
2018-11-02 13:10:14 -0400

    Ajout des conventions pour uniformiser les donnees des datagrammes

--- src/datagrams/DatagramProcessor.hpp
+++ src/datagrams/DatagramProcessor.hpp
@@ -13,8 +13,24 @@ class DatagramProcessor{
 		DatagramProcessor(){};
 		virtual ~DatagramProcessor(){};
 
+		/* Convention for attitude angles
+		 *
+		 * Pitch: Positive value is nose up
+		 * Roll: Positive value is roll to starboard (right)
+		 * Heading: gyro (magnetic north)
+		 */
                 virtual void processAttitude(uint64_t microEpoch,double heading,double pitch,double roll){};
+
+
                 virtual void processPosition(uint64_t microEpoch,double longitude,double latitude,double height){};
+
+
+		/**
+		* Convention for ping angles
+		* NED
+		* Beam angle: NEGATIVE to port (left) side, nadir is 0 degrees, POSITIVE to starboard (right) side
+		* Tilt angle: POSITIVE forward, NEGATIVE backward
+		*/
                 virtual void processPing(uint64_t microEpoch,long id, double beamAngle,double tiltAngle,double twoWayTravelTime,uint32_t quality,uint32_t intensity){};
                 virtual void processSwathStart(double surfaceSoundSpeed){};
 };
