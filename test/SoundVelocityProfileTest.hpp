3b02c899ff1543f52aacfdc1d3e3168bdf0d49b1
emilegagne <emile.gagne@cidco.ca>
2019-04-11 12:31:35 -0400

    Développement des tests sur SoundVelocityProfileTest.hpp

--- test/SoundVelocityProfileTest.hpp
+++ test/SoundVelocityProfileTest.hpp
@@ -16,9 +16,20 @@
 #include "../src/SoundVelocityProfile.hpp"
 #include "../src/utils/Exception.hpp"
 
+/**Test the writing and the reading of a SVP file*/
 TEST_CASE("Writing to SVP file"){
 	//TODO: test prototype
-	REQUIRE(1==2);
+    SoundVelocityProfile svp = SoundVelocityProfile(0,0,0);
+    svp.add(0,0);
+    svp.write("test");
+    std::string cont;
+    std::string contAttendu;
+    contAttendu << "[SVP_VERSION_2]" << "\r\n";
+    contAttendu << "test" << "\r\n";
+    contAttendu << "Section " << "0-0 0:0:0" << " " << "0:0:0" << " " << "0:0:0" << "\r\n" ;
+    contAttendu << "0 0";
+    cont = svp.read("test");
+	REQUIRE(contAttendu = cont);
 };
 
 TEST_CASE("Get speeds/depths"){
