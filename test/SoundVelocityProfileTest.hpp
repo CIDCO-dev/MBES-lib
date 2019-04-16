9afa006fa2fbeba6d54ee2cbb8484187dd0042bc
emilegagne <emile.gagne@cidco.ca>
2019-04-16 14:36:22 -0400

    Ajout de test (fonctionnelle) SoundVelocityProfileTest.hpp

--- test/SoundVelocityProfileTest.hpp
+++ test/SoundVelocityProfileTest.hpp
@@ -42,7 +42,7 @@ TEST_CASE("Test the julianTime method")
 {
   SoundVelocityProfile svp = SoundVelocityProfile();
   svp.setTimestamp(0);
-  std::string cont = svp.julianTime();
+  std::string cont = svp.julianTime(svp.getTimestamp());
   REQUIRE(cont.compare("1970-1 0:0:0")==0);
 }
 
@@ -87,3 +87,71 @@ TEST_CASE("Get speeds/depths"){
     REQUIRE(svp.getSpeeds()(0) == 5);
 }
 
+/**Test if the svp file 1st line is wrong*/
+TEST_CASE("Read the 1st line who is not valid")
+{
+    SoundVelocityProfile svp = SoundVelocityProfile();
+    std::string file = "erreur1.txt";
+    bool valide = svp.read(file);
+    REQUIRE(valide==false);
+}
+
+/**Test if the svp file 2nd line is wrong*/
+TEST_CASE("Read the 2nd line who is not valid")
+{
+    SoundVelocityProfile svp = SoundVelocityProfile();
+    std::string file = "erreur2.txt";
+    bool valide = svp.read(file);
+    REQUIRE(valide==false);
+}
+
+/**Test if the svp file 3rd line is wrong*/
+TEST_CASE("Use readTimeLatLong with a wrong row")
+{
+    SoundVelocityProfile svp = SoundVelocityProfile();
+    std::string text = "Sectin 1970-1 0:0:0 South 1:0:0 West 1:0:0";
+    uint64_t time = 0;
+    double lat = 0;
+    double lon = 0;
+    bool valide1 = svp.readTimeLatLong(text,time,lat,lon);
+    text = "Section 1970-1 00:0 South 1:0:0 West 1:0:0";
+    bool valide2 = svp.readTimeLatLong(text,time,lat,lon);
+    text = "Section 1970-1 0:0:0 Suth 1:0:0 West 1:0:0";
+    bool valide3 = svp.readTimeLatLong(text,time,lat,lon);
+    text = "Section 1970-1 0:0:0 South 1:00 West 1:0:0";
+    bool valide4 = svp.readTimeLatLong(text,time,lat,lon);
+    text = "Section 1970-1 0:0:0 South 1:0:0 Wst 1:0:0";
+    bool valide5 = svp.readTimeLatLong(text,time,lat,lon);
+    REQUIRE(valide1==false);
+    REQUIRE(valide2==false);
+    REQUIRE(valide3==false);
+    REQUIRE(valide4==false);
+    REQUIRE(valide5==false);
+}
+
+/**Test if deph and speed line is wrong*/
+TEST_CASE("Read a svp file with wrong deph and speed line")
+{
+    SoundVelocityProfile svp = SoundVelocityProfile();
+    std::string file = "erreur3.txt";
+    bool valide = svp.read(file);
+    REQUIRE(valide==false);
+}
+
+/**Test if the file doesn't exist*/
+TEST_CASE("Read a file who doesn't exist")
+{
+    SoundVelocityProfile svp = SoundVelocityProfile();
+    std::string file = "hsadhahdsdushdsd";
+    bool valide = svp.read(file);
+    REQUIRE(valide==false);
+}
+
+/**Test if the file have invalid character*/
+TEST_CASE("Read a svp file with invalid character")
+{
+    SoundVelocityProfile svp = SoundVelocityProfile();
+    std::string file = "erreur4.txt";
+    bool valide = svp.read(file);
+    REQUIRE(valide==false);
+}
