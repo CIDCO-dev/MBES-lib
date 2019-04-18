27070fb6308ba302ed470ee85a2b312f5bb37e74
emilegagne <emile.gagne@cidco.ca>
2019-04-18 08:50:26 -0400

    Documentation SurveyLine.hpp

--- src/SurveyLine.hpp
+++ src/SurveyLine.hpp
@@ -14,15 +14,30 @@
 
 #include "Swath.hpp"
 
+/*!
+ * \brief Survey Line class
+ */
 class SurveyLine {
 public:
+    
+    /**Vector who contains the swaths of the survey line*/
     const std::vector<Swath *>* swaths;
+    
+    /**Value of the number of the survey line swaths*/
     const unsigned int swathCount;
     
+    /**
+     * Create a survey line
+     * 
+     * @param swaths Vector of the survey line swaths
+     */
     SurveyLine(std::vector<Swath *>* swaths) :
     swaths(swaths), swathCount(swaths->size()) {
     }
     
+    /**
+     * Destroy the survey line and delete all his swaths
+     */
     ~SurveyLine() {
         for(unsigned int i=0; i<swathCount; i++) {
             delete (*swaths)[i];
@@ -31,6 +46,12 @@ public:
         delete swaths;
     };
     
+    /**
+     * Return a line text for each SurveyLine swath
+     * 
+     * @param os value where the lines will be put
+     * @param obj Survey Line who we need to get the swaths
+     */
     friend std::ostream& operator<<(std::ostream& os, const SurveyLine& obj) {
         for(unsigned int i=0; i<obj.swathCount; i++) {
             os << *((*obj.swaths)[i]) << std::endl;
