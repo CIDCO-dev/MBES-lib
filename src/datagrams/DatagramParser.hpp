d34d9632ebff92f22ac63362e927ae16c99aefe0
emilegagne <emile.gagne@cidco.ca>
2019-04-23 09:33:51 -0400

    Documentation DatagramParser.hpp

--- src/datagrams/DatagramParser.hpp
+++ src/datagrams/DatagramParser.hpp
@@ -4,18 +4,41 @@
 #include <cstdint>
 #include "DatagramProcessor.hpp"
 
+/*!
+ * \brief Datagram parser class
+ */
 class DatagramParser{
 	public:
+                /**
+                 * Create a datagram parser
+                 * 
+                 * @param processor the datagram processor
+                 */
 		DatagramParser(DatagramProcessor & processor);
+                
+                /**
+                 * Destroy the datagram parser
+                 */
 		virtual ~DatagramParser(){};
 
+                /**
+                 * Read a file and change the datagram parser depending on the information
+                 * 
+                 * @param filename name of the file to read
+                 */
         	virtual void parse(std::string & filename){};
 
 	protected:
 
+            /**The datagram processor*/
 		DatagramProcessor & processor;
 };
 
+/**
+ * Create a datagram parser
+ * 
+ * @param processor the datagram processor
+ */
 DatagramParser::DatagramParser(DatagramProcessor & processor) : processor(processor){
 
 }
