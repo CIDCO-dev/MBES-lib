1e734892189e704648bdf11aadc9f765359a61a1
glabmoris <guillaume.morissette@cidco.ca>
2019-04-29 15:20:54 -0400

    Ajout d'une factory method a DatagramParser

--- src/datagrams/DatagramParser.hpp
+++ src/datagrams/DatagramParser.hpp
@@ -28,6 +28,28 @@ class DatagramParser{
                  */
         	virtual void parse(std::string & filename){};
 
+		/**
+		 * Creates the appropriate parser for the given file. Throws exception for unknown formats
+		 * @param filename the name of the file
+		 */
+		static DatagramParser * build(std::string & filename){
+			DatagramParser * parser;
+
+			if(ends_with(fileName.c_str(),".all")){
+                		parser = new KongsbergParser(cloud);
+        		}
+        		else if(ends_with(fileName.c_str(),".xtf")){
+                		parser = new XtfParser(cloud);
+        		}
+        		else if(ends_with(fileName.c_str(),".s7k")){
+                		parser = new S7kParser(cloud);
+        		}
+        		else{
+                		throw new Exception("Unknown extension");
+        		}
+			return parser;
+		}
+
 	protected:
 
             /**The datagram processor*/
