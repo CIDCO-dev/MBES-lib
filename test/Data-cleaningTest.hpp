acfd2400a0f7d5c1345a18ab5296c3ca00621a80
emilegagne <emile.gagne@cidco.ca>
2019-04-29 15:38:36 -0400

    Ajout de l'execution de data-cleaning dans data-cleaningTest.hpp

--- test/Data-cleaningTest.hpp
+++ test/Data-cleaningTest.hpp
@@ -11,10 +11,55 @@
  * Created on April 29, 2019, 2:22 PM
  */
 
-#ifndef DATA_CLEANINGTEST_HPP
-#define DATA_CLEANINGTEST_HPP
+#include <Eigen/Dense>
+#include <Eigen/src/Core/Matrix.h>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include "catch.hpp"
+#include "../src/utils/Exception.hpp"
+using namespace std;
+#ifdef _WIN32
+static string binexec("..\\bin\\georeference.exe");
+static string outputdir(".");
+#else
+static string binexec("build/bin/data-cleaning");
+static string outputdir(".");
+#endif
 
+/**
+ * Execute a main function
+ * 
+ * @param command the parameters for the execution
+ */
+std::stringstream system_call(const std::string& command){
 
+     std::stringstream out;
+     FILE *fp;
+     char path[1035];
 
-#endif /* DATA_CLEANINGTEST_HPP */
+#ifdef _WIN32
+     fp = _popen(command.c_str(), "r");
+#else
+     fp = popen(command.c_str(), "r");
+#endif
+     if (fp == NULL) {
+	printf("Failed to run command\n" );
+	exit(1);
+     }
+
+     while (fgets(path, sizeof(path)-1, fp) != NULL) {
+	     out<<path;
+     }
+
+#ifdef _WIN32
+     _pclose(fp);
+#else
+     pclose(fp);
+#endif
+
+     return out;
+}
 
