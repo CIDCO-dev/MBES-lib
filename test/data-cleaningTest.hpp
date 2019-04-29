c6afa2fbb73c6cdc9e1b8e92355c04f25eb259d1
emilegagne <emile.gagne@cidco.ca>
2019-04-29 14:22:56 -0400

    Ajout de data-cleaningTest.hpp

---
+++ test/data-cleaningTest.hpp
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   data-cleaningTest.hpp
+ * Author: emile
+ *
+ * Created on April 29, 2019, 2:22 PM
+ */
+
+#ifndef DATA_CLEANINGTEST_HPP
+#define DATA_CLEANINGTEST_HPP
+
+
+
+#endif /* DATA_CLEANINGTEST_HPP */
+
