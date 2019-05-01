0af69af9d23685ff0d3f5797f18c070cfd09b160
emilegagne <emile.gagne@cidco.ca>
2019-05-01 13:26:53 -0400

    Ajout du filtre InsanePosition dans data-cleaning.cpp

--- src/examples/data-cleaning.cpp
+++ src/examples/data-cleaning.cpp
@@ -134,6 +134,42 @@ class IntensityFilter : public PointFilter{
 
 };
 
+/*!
+ * \brief Insane position filter class extend of the Point filter class
+ */
+class InsanePositionFilter : public PointFilter{
+   public:
+
+       /**
+        * Create a insane position filter
+        */
+	IntensityFilter(){
+
+	}
+
+        /**Destroy the insane position filter*/
+	~IntensityFilter(){
+
+	}
+
+        /**
+         * return true if the position receive seem insane
+         * 
+         * @param microEpoch timestamp of the point
+         * @param x x position of the point
+         * @param y y position of the point
+         * @param z z position of the point
+         * @param quality quality of the point
+         * @param intensity intensity of the point
+         */
+	bool filterPoint(uint64_t microEpoch,double x,double y,double z, uint32_t quality,uint32_t intensity){
+            bool insaneX = ((x>1.00*100000000)||(x<-1.00*100000000));
+            bool insaneY = ((y>1.00*100000000)||(y<-1.00*100000000));
+            bool insaneZ = ((z>1.00*100000000)||(z<-1.00*100000000));
+            return (insaneX||insaneY||insaneZ);
+	}
+};
+
 /**
  * Filter all point receive in the terminal
  * 
@@ -146,6 +182,7 @@ int main(int argc,char** argv){
 
 	//Filter chain
 	std::list<PointFilter *> filters;
+        filters.push_back(new InsanePositionFilter());
 
 	//TODO: load desired filters and parameters from command line
         int index;
