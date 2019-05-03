d43f329122ea7dfb84a8f2219c915ddc39625f37
emilegagne <emile.gagne@cidco.ca>
2019-05-03 14:05:13 -0400

    Ajout du fichier KongsbergParserTest.hpp

---
+++ test/KongsbergParserTest.hpp
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   KongsbergParserTest.hpp
+ * Author: emile
+ *
+ * Created on May 3, 2019, 2:02 PM
+ */
+
+#ifndef KONGSBERGPARSERTEST_HPP
+#define KONGSBERGPARSERTEST_HPP
+
+
+
+#endif /* KONGSBERGPARSERTEST_HPP */
+
