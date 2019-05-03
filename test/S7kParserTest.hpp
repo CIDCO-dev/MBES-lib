b61469e40395eaa938cd7581cd2fb95a2e488eca
emilegagne <emile.gagne@cidco.ca>
2019-05-03 15:22:41 -0400

    Ajout de test S7kParserTest.hpp

--- test/S7kParserTest.hpp
+++ test/S7kParserTest.hpp
@@ -10,11 +10,20 @@
  *
  * Created on May 3, 2019, 2:03 PM
  */
+#include "catch.hpp"
+#include "../src/datagrams/DatagramEventHandler.hpp"
+#include "../src/datagrams/s7k/S7kParser.hpp"
 
-#ifndef S7KPARSERTEST_HPP
-#define S7KPARSERTEST_HPP
-
-
-
-#endif /* S7KPARSERTEST_HPP */
-
+TEST_CASE("test the function getName")
+{
+    DatagramEventHandler handler;
+    S7kParser parser(handler);
+    REQUIRE(parser.getName(1004)=="Custom Attitude Information");
+    REQUIRE(parser.getName(1020)=="Sonar Installation Identifiers");
+    REQUIRE(parser.getName(2000)=="Invalid tag");
+    REQUIRE(parser.getName(7000)=="7k Sonar Settings");
+    REQUIRE(parser.getName(7007)=="7k Side Scan Data");
+    REQUIRE(parser.getName(7030)=="Sonar Installation Parameters");
+    REQUIRE(parser.getName(7050)=="7k System Events");
+    REQUIRE(parser.getName(7700)=="Invalid tag");
+}
