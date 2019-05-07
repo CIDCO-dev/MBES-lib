4326d12bea46a1ebb282c9d48fbe03bdf809af5b
emilegagne <emile.gagne@cidco.ca>
2019-05-07 16:11:28 -0400

    Correction test BoresightTest.hpp

--- test/BoresightTest.hpp
+++ test/BoresightTest.hpp
@@ -34,16 +34,10 @@ TEST_CASE("Test with the angle 1,2,3")
     Attitude boresightAngles(0,1,2,3);
     Eigen::Matrix3d boresight;
     Boresight::buildMatrix(boresight,boresightAngles);
-    double ch = cos(3*D2R);
-    double sh = sin(3*D2R);
-    double cp = cos(2*D2R);
-    double sp = sin(2*D2R);
-    double cr = cos(1*D2R);
-    double sr = sin(1*D2R);
     Eigen::Matrix3d matrixSearch;
-    matrixSearch << ch*cp , ch*sp*sr - sh*cr  , ch*sp*cr + sh*sr,
-                    sh*cp , sh*sp*sr + ch*cr  , sh*sp*cr - ch*sr,
-                    -sp   , cp*sr             , cp*cr;
+    matrixSearch << 0.998021196624068,-0.0517197397456515,0.0357597484569552,
+                    0.0523040745924708,0.998509315434203,-0.0156022681730626,
+                    -0.0348994967025010,0.0174417749028302,0.999238614955483;
     REQUIRE(boresight == matrixSearch);
 }
 
@@ -52,15 +46,9 @@ TEST_CASE("Test with the angle 10,26,39")
     Attitude boresightAngles(0,10,26,39);
     Eigen::Matrix3d boresight;
     Boresight::buildMatrix(boresight,boresightAngles);
-    double ch = cos(39*D2R);
-    double sh = sin(39*D2R);
-    double cp = cos(26*D2R);
-    double sp = sin(26*D2R);
-    double cr = cos(10*D2R);
-    double sr = sin(10*D2R);
     Eigen::Matrix3d matrixSearch;
-    matrixSearch << ch*cp , ch*sp*sr - sh*cr  , ch*sp*cr + sh*sr,
-                    sh*cp , sh*sp*sr + ch*cr  , sh*sp*cr - ch*sr,
-                    -sp   , cp*sr             , cp*cr;
+    matrixSearch << 0.698494163262967,-0.560601422747957,0.444783035535905,
+                    0.565629420690258,0.813244715626515,0.136734746700144,
+                    -0.438371146789077,0.156073948237737,0.885139345156633;
     REQUIRE(boresight == matrixSearch);
 }
