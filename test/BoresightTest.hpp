d72a3ee93e2b017d7281431a6b711320557f2ad5
emilegagne <emile.gagne@cidco.ca>
2019-05-07 09:50:23 -0400

    Ajout de BoresightTest.hpp

---
+++ test/BoresightTest.hpp
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   BoresightTest.hpp
+ * Author: emile
+ *
+ * Created on May 7, 2019, 9:49 AM
+ */
+
+#ifndef BORESIGHTTEST_HPP
+#define BORESIGHTTEST_HPP
+
+
+
+#endif /* BORESIGHTTEST_HPP */
+
