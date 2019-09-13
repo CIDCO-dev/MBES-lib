8457022be6502d85f517692168582f310dd54ac4
glabmoris <guillaume.morissette@cidco.ca>
2019-09-13 16:16:04 -0400

    Added Haversine doc and fixed units

--- src/math/Distance.hpp
+++ src/math/Distance.hpp
@@ -17,6 +17,14 @@
 class Distance{
 public:
     
+    /**
+     * Gives the distance in meters between two points on a spheroid 
+     * @param longitude1 Point1's longitude
+     * @param latitude1 Point1's latitude
+     * @param longitude2 Point2's longitude
+     * @param latitude2 Point2's latitude
+     * @return the distance in meters
+     */
     static double haversine(double longitude1, double latitude1, double longitude2, double latitude2){
 
 	double dx, dy, dz;
@@ -26,7 +34,7 @@ public:
 	dz = sin(longitude1) - sin(longitude2);
 	dx = cos(latitude1) * cos(longitude1) - cos(longitude2);
 	dy = sin(latitude1) * cos(longitude1);
-	return asin(sqrt(dx * dx + dy * dy + dz * dz) / 2) * 2 * 6371;    
+	return asin(sqrt(dx * dx + dy * dy + dz * dz) / 2) * 2 * 6371000;    
     }
 };
 
