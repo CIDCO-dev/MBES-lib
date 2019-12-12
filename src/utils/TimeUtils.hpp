020e7c41c964302d6f3e60f5f3c1414a887d9d2e
JordanMcManus <jordan.mcmanus@cidco.ca>
2019-12-12 16:23:43 -0500

    more testing of windows strptime

--- src/utils/TimeUtils.hpp
+++ src/utils/TimeUtils.hpp
@@ -29,16 +29,7 @@ public:
      */
 
 #ifdef _WIN32
-
-    extern "C" char* strptime(const char* s,
-            const char* f,
-            struct tm* tm) {
-        // Isn't the C++ standard lib nice? std::get_time is defined such that its
-        // format parameters are the exact same as strptime. Of course, we have to
-        // create a string stream first, and imbue it with the current C locale, and
-        // we also have to make sure we return the right things if it fails, or
-        // if it succeeds, but this is still far simpler an implementation than any
-        // of the versions in any of the C standard libraries.
+    extern "C" char* strptime(const char* s, const char* f, struct tm* tm) {
         std::istringstream input(s);
         input.imbue(std::locale(setlocale(LC_ALL, nullptr)));
         input >> std::get_time(tm, f);
