8dd2fcf92ae7348b071d0e7a3efab222ada4ce91
JordanMcManus <jordan.mcmanus@cidco.ca>
2019-12-12 16:32:05 -0500

    more testing of windows strptime

--- src/utils/TimeUtils.hpp
+++ src/utils/TimeUtils.hpp
@@ -29,7 +29,7 @@ public:
      */
 
 #ifdef _WIN32
-    static char *strptime(const char * s, const char* f, struct tm* tm);
+    static char *strptime(const char * s, const char* f, struct tm* tm) {
         std::istringstream input(s);
         input.imbue(std::locale(setlocale(LC_ALL, nullptr)));
         input >> std::get_time(tm, f);
