39b406e5ca0788a56a056eebc3142f54ae5d373b
JordanMcManus <jordan.mcmanus@cidco.ca>
2020-12-16 12:54:30 -0500

    added more tests for ends_with_ci

--- test/StringUtilsTest.hpp
+++ test/StringUtilsTest.hpp
@@ -18,6 +18,14 @@ TEST_CASE("Test the String Utils ends_with_ci function") {
     std::string file_xtf = "file.xtf";
     std::string file_all = "file.all";
     
+    std::string file_s7k_cap = "file.S7K";
+    std::string file_xtf_cap = "file.XTF";
+    std::string file_all_cap = "file.ALL";
+    
+    std::string file_s7k_mixedCase = "file.s7K";
+    std::string file_xtf_mixedCase = "file.XtF";
+    std::string file_all_mixedCase = "file.alL";
+    
     std::string ext_s7k = ".s7k";
     std::string ext_xtf = ".xtf";
     std::string ext_all = ".all";
@@ -33,6 +41,30 @@ TEST_CASE("Test the String Utils ends_with_ci function") {
     REQUIRE(StringUtils::ends_with_ci(file_all.c_str(),ext_s7k.c_str())==false);
     REQUIRE(StringUtils::ends_with_ci(file_all.c_str(),ext_xtf.c_str())==false);
     REQUIRE(StringUtils::ends_with_ci(file_all.c_str(),ext_all.c_str())==true);
+    
+    REQUIRE(StringUtils::ends_with_ci(file_s7k_cap.c_str(),ext_s7k.c_str())==true);
+    REQUIRE(StringUtils::ends_with_ci(file_s7k_cap.c_str(),ext_xtf.c_str())==false);
+    REQUIRE(StringUtils::ends_with_ci(file_s7k_cap.c_str(),ext_all.c_str())==false);
+    
+    REQUIRE(StringUtils::ends_with_ci(file_xtf_cap.c_str(),ext_s7k.c_str())==false);
+    REQUIRE(StringUtils::ends_with_ci(file_xtf_cap.c_str(),ext_xtf.c_str())==true);
+    REQUIRE(StringUtils::ends_with_ci(file_xtf_cap.c_str(),ext_all.c_str())==false);
+    
+    REQUIRE(StringUtils::ends_with_ci(file_all_cap.c_str(),ext_s7k.c_str())==false);
+    REQUIRE(StringUtils::ends_with_ci(file_all_cap.c_str(),ext_xtf.c_str())==false);
+    REQUIRE(StringUtils::ends_with_ci(file_all_cap.c_str(),ext_all.c_str())==true);
+    
+    REQUIRE(StringUtils::ends_with_ci(file_s7k_mixedCase.c_str(),ext_s7k.c_str())==true);
+    REQUIRE(StringUtils::ends_with_ci(file_s7k_mixedCase.c_str(),ext_xtf.c_str())==false);
+    REQUIRE(StringUtils::ends_with_ci(file_s7k_mixedCase.c_str(),ext_all.c_str())==false);
+    
+    REQUIRE(StringUtils::ends_with_ci(file_xtf_mixedCase.c_str(),ext_s7k.c_str())==false);
+    REQUIRE(StringUtils::ends_with_ci(file_xtf_mixedCase.c_str(),ext_xtf.c_str())==true);
+    REQUIRE(StringUtils::ends_with_ci(file_xtf_mixedCase.c_str(),ext_all.c_str())==false);
+    
+    REQUIRE(StringUtils::ends_with_ci(file_all_mixedCase.c_str(),ext_s7k.c_str())==false);
+    REQUIRE(StringUtils::ends_with_ci(file_all_mixedCase.c_str(),ext_xtf.c_str())==false);
+    REQUIRE(StringUtils::ends_with_ci(file_all_mixedCase.c_str(),ext_all.c_str())==true);
 }
 
 TEST_CASE("Test the String Utils strcmpi function") {
